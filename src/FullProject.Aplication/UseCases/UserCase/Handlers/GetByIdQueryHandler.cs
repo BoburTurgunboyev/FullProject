3f7c0838dca03fad043001af679c41f47cf62a9e
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-13 17:38:01 +0500

    crud

---
+++ src/FullProject.Aplication/UseCases/UserCase/Handlers/GetByIdQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using FullProject.Aplication.Absreaction;
+using FullProject.Aplication.UseCases.UserCase.Queries;
+using FullProject.Domain.Entities.Users;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FullProject.Aplication.UseCases.UserCase.Handlers
+{
+    public class GetByIdQueryHandler : IRequestHandler<GetByIdQuery, User>
+    {
+        private readonly IDbConnect _dbConnect;
+
+        public GetByIdQueryHandler(IDbConnect dbConnect)
+        {
+            _dbConnect = dbConnect;
+        }
+
+        public async Task<User> Handle(GetByIdQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _dbConnect.users.FirstOrDefaultAsync(x => x.Id == request.Id);
+            return result;
+        }
+    }
+}
