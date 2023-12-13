edb5c3a4fefa5641854ebfdf50c268610f7e1c23
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-13 17:38:49 +0500

    crud

---
+++ src/FullProject.Aplication/Dependency/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FullProject.Aplication.Dependency
+{
+    public static  class DependencyInjection
+    {
+
+        public static IServiceCollection AddAplication(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            return services;
+
+        }
+    }
+}
