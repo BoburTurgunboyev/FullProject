a2a7fec5f0d2897e87b27eff01e74c5327010ad9
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-13 17:37:39 +0500

    crud

---
+++ src/FullProject.Aplication/UseCases/UserCase/Queries/GetAllUserQuery.cs
@@ -0,0 +1,14 @@
+﻿using FullProject.Domain.Entities.Users;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FullProject.Aplication.UseCases.UserCase.Queries
+{
+    public class GetAllUserQuery : IRequest<List<User>>
+    {
+    }
+}
