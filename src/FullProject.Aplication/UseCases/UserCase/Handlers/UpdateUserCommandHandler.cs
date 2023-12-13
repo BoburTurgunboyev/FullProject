f75aa20c8b66e8d550f9baa6f54b9cdaaf6702e2
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-13 17:37:53 +0500

    crud

---
+++ src/FullProject.Aplication/UseCases/UserCase/Handlers/UpdateUserCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using FullProject.Aplication.Absreaction;
+using FullProject.Aplication.UseCases.UserCase.Commands;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FullProject.Aplication.UseCases.UserCase.Handlers
+{
+    public class UpdateUserCommandHandler : IRequestHandler<UpdateUserCommand, bool>
+    {
+        private readonly IDbConnect _dbConnection;
+
+        public UpdateUserCommandHandler(IDbConnect dbConnection)
+        {
+            _dbConnection = dbConnection;
+        }
+
+        public async Task<bool> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
+        {
+            var res = await _dbConnection.users.FirstOrDefaultAsync(x => x.Id == request.Id);
+            if (res is null)
+                return false;
+            res.Name = request.Name;
+            res.Age = request.Age;  
+            res.Password = request.Password;
+
+            _dbConnection.users.Update(res);
+            var result  = await _dbConnection.SaveChangesAsync(cancellationToken);
+            return result > 0;
+        }
+    }
+}
