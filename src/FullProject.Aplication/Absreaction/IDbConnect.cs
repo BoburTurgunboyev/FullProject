87e5f0e376675ec10f6e47e76316a4d37f0b3377
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-13 17:38:54 +0500

    crud

---
+++ src/FullProject.Aplication/Absreaction/IDbConnect.cs
@@ -0,0 +1,18 @@
+﻿using FullProject.Domain.Entities.Users;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FullProject.Aplication.Absreaction
+{
+    public interface IDbConnect
+    { 
+        public DbSet<User> users { get; set; }
+
+
+        public ValueTask<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
