f32ea8d0832e438f72bb7d76fbdd59497ed7fe22
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-13 17:36:28 +0500

    crud

---
+++ src/FullProject.Infrastucture/Data/DbConnect.cs
@@ -0,0 +1,23 @@
+﻿using FullProject.Aplication.Absreaction;
+using FullProject.Domain.Entities.Users;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FullProject.Infrastucture.Data
+{
+    public class DbConnect : DbContext, IDbConnect
+    {
+        public DbConnect(DbContextOptions<DbConnect> options) : base(options) { }
+       
+        public DbSet<User> users { get ; set ; }
+
+        async ValueTask<int> IDbConnect.SaveChangesAsync(CancellationToken cancellationToken)
+        {
+            return await base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
