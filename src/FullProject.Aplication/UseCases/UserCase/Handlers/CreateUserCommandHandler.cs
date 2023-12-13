342b800d6aed4fce8e20d9337a3f2d8f19cf9696
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-13 17:38:18 +0500

    crud

---
+++ src/FullProject.Aplication/UseCases/UserCase/Handlers/CreateUserCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using FullProject.Aplication.Absreaction;
+using FullProject.Aplication.UseCases.UserCase.Commands;
+using FullProject.Domain.Entities.Users;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FullProject.Aplication.UseCases.UserCase.Handlers
+{
+    public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, bool>
+    {
+        private readonly IDbConnect _dbConnect;
+
+        public CreateUserCommandHandler(IDbConnect dbConnect)
+        {
+            _dbConnect = dbConnect;
+        }
+
+        public async Task<bool> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+        {
+            var user = new User()
+            {
+                Name = request.Name,
+                Age = request.Age,
+                Password = request.Password,
+            };
+
+            _dbConnect.users.Add(user);
+            var res = await _dbConnect.SaveChangesAsync(cancellationToken);
+            return res > 0;
+        }
+    }
+}
