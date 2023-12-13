3524d904370c098d3fd74378cfd7989a1bb29445
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-13 17:38:24 +0500

    crud

---
+++ src/FullProject.Aplication/UseCases/UserCase/Dtos/UserDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FullProject.Aplication.UseCases.UserCase.Dtos
+{
+    public class UserDto
+    {
+        
+        public string Name { get; set; }
+        public string Age { get; set; }
+        public string Password { get; set; }
+    }
+}
