d369b5ba99dbb53e8cd7d2a7f600304b2d56d1f2
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-13 17:36:23 +0500

    crud

---
+++ src/FullProject.Infrastucture/Dependencyy/DependencyyInjection.cs
@@ -0,0 +1,26 @@
+﻿using FullProject.Aplication.Absreaction;
+using FullProject.Infrastucture.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FullProject.Infrastucture.Dependencyy
+{
+    public static class DependencyyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<IDbConnect, DbConnect>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            return services;
+        }
+    }
+}
