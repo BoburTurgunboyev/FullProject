a0296c5b4d107b93e233505cb29c003cff2da9b5
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-13 17:37:06 +0500

    crud

---
+++ src/FullProject.Domain/Entities/Users/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FullProject.Domain.Entities.Users
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Age { get; set; }
+        public string Password { get; set; }
+    }
+}
