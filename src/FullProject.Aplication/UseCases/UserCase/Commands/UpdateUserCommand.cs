42102df4fd30ba19bf1c3a2e095814928e6422a9
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-13 17:38:32 +0500

    crud

---
+++ src/FullProject.Aplication/UseCases/UserCase/Commands/UpdateUserCommand.cs
@@ -0,0 +1,15 @@
+﻿using FullProject.Aplication.UseCases.UserCase.Dtos;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FullProject.Aplication.UseCases.UserCase.Commands
+{
+    public class UpdateUserCommand : UserDto ,IRequest<bool>
+    {
+        public int Id { get; set; }
+    }
+}
