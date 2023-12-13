ac157e7fdcadec1151546cee9f3d021fd01b93eb
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-13 17:38:12 +0500

    crud

---
+++ src/FullProject.Aplication/UseCases/UserCase/Handlers/DeleteUserCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using FullProject.Aplication.Absreaction;
+using FullProject.Aplication.UseCases.UserCase.Commands;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FullProject.Aplication.UseCases.UserCase.Handlers
+{
+    public class DeleteUserCommandHandler : IRequestHandler<DeleteUserCommand, bool>
+    {
+        private readonly IDbConnect _dbConnection;
+
+        public DeleteUserCommandHandler(IDbConnect dbConnection)
+        {
+            _dbConnection = dbConnection;
+        }
+
+        public async Task<bool> Handle(DeleteUserCommand request, CancellationToken cancellationToken)
+        {
+            var result  = await _dbConnection.users.FirstOrDefaultAsync(x =>  x.Id == request.Id);
+            if (result == null)
+            {
+                return false;
+            }
+
+            _dbConnection.users.Remove(result); 
+            var res = await _dbConnection.SaveChangesAsync(cancellationToken);
+            return res > 0;
+        }
+    }
+}
